aea09dc3dbf3d3bc43b19a2b0baa157039449e19
Alexander Nickel <mr.alexander.nickel@gmail.com>
2022-10-04 12:45:31 +0200

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 export BW_CLIENTID=$INPUT_BW_CLIENTID
 export BW_CLIENTSECRET=$INPUT_BW_CLIENTSECRET
-printenv
+#printenv
 bw login --apikey
+export BW_SESSION=$(bw unlock --raw --passwordenv INPUT_BW_PASSWORD)
+bw list items
 time=$(date)
 echo "::set-output name=time::$time"
